1d9a03303902c1979a0457e1119f1614a810bb31
SunilKumarPradhan <sunilkumarweb47@gmail.com>
2023-03-20 19:49:32 +0530

    UI works but cannot get post request

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 const express = require("express");
+const bodyParser=require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/signup.html");
+});
+app.post(("/",(req,res)=>{
+var firstName=req.body.fName;
+var lastName=req.body.lName;
+var email=req.body.email; 
+
+console.log(firstName,lastName,email);
+}));
+
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+}); 
