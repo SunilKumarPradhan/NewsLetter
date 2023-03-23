739ebe326282d0d749e364a19fafcae449a88230
SunilKumarPradhan <sunilkumarweb47@gmail.com>
2023-03-23 21:44:46 +0530

    Final Push

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post("/", (req, res) => {
     res.redirect("/");
   });
   
-  app.listen(process.env.PORT || 3000, () => {
+  app.listen(process.env.PORT || 3000 , () => {
     console.log("Server is running on port 3000");
   });
 
